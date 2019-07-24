a9b7b6a90dfdd88fac453fcb8832ab12058e78bf
Joe <josephgbuckley@gmail.com>
2019-07-24 23:44:20 +0100

    added some graphing

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import pingouin as pg
 
 data_table = pd.read_csv('data.csv') 
+
+data_table.groupby('Age').size().plot.bar(color='Blue')	
+plt.xlabel('Age')
+plt.ylabel('Sample Size')
+plt.tight_layout()
+plt.show()
+sample = data_table.groupby('Age').size()
+print(sample.mean())
+print(sample.std())
+print(sample.min())
+print(sample.max())
+print(s)
+
+data_table.boxplot(column='Age',by='Gender',showmeans=True)
+plt.show()
+print("hello world")
+
